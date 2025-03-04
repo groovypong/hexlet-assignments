c07860bc77df67741539cf609da218a664c4e3af
@hexlet/cli <support@hexlet.io>
2025-03-04 14:12:46 +0300

    submit java-spring-boot-ru/rest-api

--- java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -3,7 +3,6 @@ package exercise.controller.users;
 import exercise.Data;
 import exercise.model.Post;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
